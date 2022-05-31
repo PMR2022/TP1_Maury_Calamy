b6c25881f7396be14cb34d974b25d90ee10843cc
victor-calamy <victor.calamy@gmail.com>
2022-05-31 17:47:19 +0200

    preferences pseudo

--- app/src/main/java/com/example/tp1_maury_calamy/MainActivity.kt
+++ app/src/main/java/com/example/tp1_maury_calamy/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +30,28 @@ class MainActivity : AppCompatActivity() {
             val choixListActivity = Intent(this,ChoixListActivity::class.java)
             startActivity(choixListActivity)
         }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.getItemId()
+
+        if (id == R.id.action_one) {
+            val settingActivity = Intent(this,SettingActivity::class.java)
+            startActivity(settingActivity)
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+
     }
 }
+
